c7ea382c910c65e8ef0930a3781d33f7bc3c7db6
Moises Pahati <moisespahati@gmail.com>
2023-08-03 23:47:15 +0800

    add more starter files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import "./index.css";
 
 function App() {
   return (
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
-import PageNav from "../components/PageNav";
-
 export default function PageNotFound() {
   return (
     <div>
-      <PageNav /> Not Found :( Error 404
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
